07a0583124e477621bd0e5105c2aa00ffbd42ae1
Orest Kostiuk <xxmolekula@gmail.com>
2018-02-23 12:21:40 +0200

    add Roles

--- web/models/user.ex
+++ web/models/user.ex
@@ -7,7 +7,10 @@ defmodule Pxblog.User do
     field :username, :string
     field :email, :string
     field :password_digest, :string
+    
+    
     has_many :posts, Pxblog.Post
+    belongs_to :role, Pxblog.Role
 
     
     # Virtual Fields
@@ -22,8 +25,8 @@ defmodule Pxblog.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:username, :email, :password, :password_confirmation])
-    |> validate_required([:username, :email, :password, :password_confirmation])
+    |> cast(params, [:username, :email, :password, :password_confirmation, :role_id])
+    |> validate_required([:username, :email, :password, :password_confirmation,  :role_id])
     |> hash_password
   end
 
