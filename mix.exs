fb95b0ee2dc2ea769ca5a0175d6aaa257e4aaf19
Orest Kostiuk <xxmolekula@gmail.com>
2018-02-23 13:03:09 +0200

    add MarkDown to app

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Pxblog.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-	    {:comeonin, "~> 2.3"}
+	    {:comeonin, "~> 2.3"},
+	    {:earmark, "~> 1.0.1"}
     ]
   end
 
